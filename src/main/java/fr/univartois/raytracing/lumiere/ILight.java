a9b310e3d46a0ffd94139e39f144738bb90324d9
Axel <axelpoteau19@gmail.com>
2023-10-23 17:14:51 +0200

    documentation for the Light package

--- src/main/java/fr/univartois/raytracing/lumiere/ILight.java
+++ src/main/java/fr/univartois/raytracing/lumiere/ILight.java
@@ -2,9 +2,30 @@ package fr.univartois.raytracing.lumiere;
 
 import fr.univartois.raytracing.numeric.Color;
 import fr.univartois.raytracing.numeric.Point;
+import fr.univartois.raytracing.numeric.Vector;
 
+/**
+ * The ILight interface defines the contract for all types of light sources in a ray tracing system.
+ */
 public interface ILight {
-    Point getCoord();
+    /**
+     * Get the direction vector of the light source.
+     *
+     * @return direction vector representing the light's direction.
+     */
+    Vector getVector();
 
+    /**
+     * Get the color of the light source.
+     *
+     * @return color of the light.
+     */
     Color getColor();
+
+    /**
+     * Get the position of the light source (if applicable, otherwise return null).
+     *
+     * @return position of the light source (or null if the light has no specific position).
+     */
+    Point getCoord();
 }
--- src/main/java/fr/univartois/raytracing/lumiere/PonctualLight.java
+++ src/main/java/fr/univartois/raytracing/lumiere/PonctualLight.java
@@ -3,21 +3,53 @@ package fr.univartois.raytracing.lumiere;
 
 import fr.univartois.raytracing.numeric.Color;
 import fr.univartois.raytracing.numeric.Point;
+import fr.univartois.raytracing.numeric.Vector;
 
+/**
+ * The PonctualLight class represents a ponctual light source in a ray tracing system. It emits light
+ * of a specific color and is defined by a direction vector.
+ */
 public class PonctualLight implements ILight{
-    Color color;
-    Point coord;
+    Color color; // color of the light
+    Point coord; // coordonate of the light
 
+
+    /**
+     * Constructs a new DirectionalLight object with the specified color and direction vector.
+     *
+     * @param color  color of the directional light.
+     * @param coord  coord of the light.
+     */
     public PonctualLight(Color color, Point coord) {
         this.color = color;
         this.coord = coord;
     }
 
+    /**
+     * Get the position of the light (null for directional lights).
+     *
+     * @return position of the light (null for directional lights).
+     */
     @Override
     public Point getCoord() {
         return this.coord;
     }
 
+    /**
+     * Get the direction vector of the light.
+     *
+     * @return direction vector of the light source.
+     */
+    @Override
+    public Vector getVector() {
+        return null;
+    }
+
+    /**
+     * Get the color of the light.
+     *
+     * @return color of the light source.
+     */
     @Override
     public Color getColor() {
         return this.color;
