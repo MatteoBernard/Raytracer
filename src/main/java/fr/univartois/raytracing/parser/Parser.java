2bed0770ee40af375a74e0a64c557753e8c079da
Matteo Bernard <matteo_bernard1@ens.univ-artois.fr>
2023-10-23 17:11:20 +0200

    fixing Parser : finishing the processFile, and change pattern (Parser not a Singleton anymore)

--- src/main/java/fr/univartois/raytracing/parser/Parser.java
+++ src/main/java/fr/univartois/raytracing/parser/Parser.java
@@ -1,20 +1,40 @@
 package fr.univartois.raytracing.parser;
 
+import fr.univartois.raytracing.lumiere.DirectionalLight;
+import fr.univartois.raytracing.lumiere.ILight;
+import fr.univartois.raytracing.lumiere.PonctualLight;
+import fr.univartois.raytracing.numeric.Color;
+import fr.univartois.raytracing.numeric.Point;
+import fr.univartois.raytracing.numeric.Triplet;
+import fr.univartois.raytracing.numeric.Vector;
+import fr.univartois.raytracing.scenery.Camera;
+import fr.univartois.raytracing.shape.IShape;
+import fr.univartois.raytracing.shape.Plane;
+import fr.univartois.raytracing.shape.Sphere;
+import fr.univartois.raytracing.shape.Tri;
+
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 
 public final class Parser {
 
     private Scanner scanner;
-
     private static Parser instance;
+    private HashMap<String, Integer> expectedParams;
+    private HashMap<String, Color> colors;
+    private List<IShape> shapes;
+    private Integer[] dimensions;
+    private List<ILight> lights;
+    private Point[] points;
+    private int nbPoints;
+    private Camera camera;
+    private int shininess;
+    private int maxverts;
+    private String output;
 
-    private static HashMap<String, Integer> expectedParams;
-
-    private Parser() {
+    // Constructor
+    public Parser() {
         expectedParams = new HashMap<String, Integer>();
         expectedParams.put("sphere", 4);
         expectedParams.put("vertex", 3);
@@ -30,6 +50,16 @@ public final class Parser {
         expectedParams.put("directionnal", 6);
         expectedParams.put("point", 6);
         expectedParams.put("maxverts", 1);
+
+        this.shapes = new ArrayList<IShape>();
+        this.maxverts = -1;
+        this.shininess = -1;
+        this.lights = new ArrayList<ILight>();
+        this.output = "";
+        this.dimensions = new Integer[]{-1, -1};
+        this.camera = null;
+        this.nbPoints = -1;
+        this.colors = new HashMap<String, Color>();
     }
 
     public static Parser getInstance() {
@@ -38,11 +68,46 @@ public final class Parser {
         return instance;
     }
 
-    public void openFile(String fileName) throws FileNotFoundException {
-        File file =  new File(fileName);
-        this.scanner = new Scanner(file);
+    public HashMap<String, Color> getColors() {
+        return colors;
     }
 
+    public List<IShape> getShapes() {
+        return shapes;
+    }
+
+    public Integer[] getDimensions() {
+        return dimensions;
+    }
+
+    public List<ILight> getLights() {
+        return lights;
+    }
+
+    public Point[] getPoints() {
+        return points;
+    }
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public int getShininess() {
+        return shininess;
+    }
+
+    public int getMaxverts() {
+        return maxverts;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void openFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        this.scanner = new Scanner(file);
+    }
     public void closeFile() {
         this.scanner.close();
     }
@@ -54,8 +119,9 @@ public final class Parser {
         return line.charAt(0) != '#';
     }
 
-    public void processFile() {
+    public void processFile() throws Exception {
         String line = null;
+
         while (scanner.hasNextLine()) {
             line = scanner.nextLine();
 
@@ -69,16 +135,119 @@ public final class Parser {
 
                         if (parts.length - 1 == expectedParamCount) {
 
-                            System.out.println("Attribut : " + attribute);
-                            for (int i = 1; i < parts.length; i++) {
-                                double arg = Double.parseDouble(parts[i]);
-                                System.out.println("Argument " + i + " : " + arg);
+                            switch (attribute) {
+
+                                case "size" : {
+                                    this.dimensions = new Integer[]{Integer.valueOf(parts[1]), Integer.valueOf(parts[2])};
+                                    break;
+                                }
+                                case "output" : {
+                                    this.output = parts[1];
+                                    break;
+                                }
+                                case "camera" : {
+                                    Point lookFrom = new Point(new Triplet(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3])));
+                                    Point lookAt = new Point(new Triplet(Integer.parseInt(parts[4]), Integer.parseInt(parts[5]), Integer.parseInt(parts[6])));
+                                    Point up = new Point(new Triplet(Integer.parseInt(parts[7]), Integer.parseInt(parts[8]), Integer.parseInt(parts[9])));
+                                    this.camera = new Camera(lookFrom, lookAt, up, Integer.valueOf(parts[10]));
+                                    break;
+                                }
+                                case "ambient" : {
+                                    boolean good = true;
+                                    Color c = new Color(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3])));
+                                    if (colors.containsKey("diffuse")) {
+                                        if (colors.get("diffuse").getTriplet().getX() + Double.parseDouble(parts[1]) >1 ||
+                                                colors.get("diffuse").getTriplet().getY() + Double.parseDouble(parts[2]) >1 ||
+                                                colors.get("diffuse").getTriplet().getZ() + Double.parseDouble(parts[3]) >1)
+                                            good = false;
+                                        if (good) {
+                                            colors.put("ambient", c);
+                                        }
+                                        else
+                                            throw new Exception("Incorrect entry (ambient)");
+                                    }
+                                    else {
+                                        colors.put("ambient", c);
+                                    }
+                                    break;
+                                }
+                                case "diffuse" : {
+                                    boolean good = true;
+                                    Color c = new Color(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3])));
+                                    if (colors.containsKey("ambient")) {
+                                        if (colors.get("ambiant").getTriplet().getX() + Double.parseDouble(parts[1]) >1 ||
+                                                colors.get("ambiant").getTriplet().getY() + Double.parseDouble(parts[2]) >1 ||
+                                                colors.get("ambiant").getTriplet().getZ() + Double.parseDouble(parts[3]) >1)
+                                            good = false;
+                                        if (good) {
+                                            colors.put("diffuse", c);
+                                        }
+                                        else
+                                            throw new Exception("Incorrect entry (diffuse)");
+                                    }
+                                    else {
+                                        colors.put("diffuse", c);
+                                    }
+                                    break;
+                                }
+                                case "specular" : {
+                                    colors.put("specular", new Color(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]))));
+                                    break;
+                                }
+                                case "shininess" : {
+                                    int i = Integer.parseInt(parts[1]);
+                                    if (i >= 0)
+                                        this.shininess = i;
+                                    else
+                                        throw new Exception("Incorrect entry (shininess)");
+                                    break;
+                                }
+                                case "directional" : {
+                                    lights.add(new DirectionalLight(new Vector(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]))),
+                                            new Color(new Triplet(Double.parseDouble(parts[4]), Double.parseDouble(parts[5]), Double.parseDouble(parts[6])))));
+                                    break;
+                                }
+                                case "point" : {
+                                    lights.add(new PonctualLight(new Color(new Triplet(Double.parseDouble(parts[4]), Double.parseDouble(parts[5]), Double.parseDouble(parts[6]))),
+                                            new Point(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3])))));
+                                    break;
+                                }
+                                case "maxverts" : {
+                                    this.maxverts = Integer.parseInt(parts[1]);
+                                    this.points = new Point[this.maxverts];
+                                    this.nbPoints = 0;
+                                    break;
+                                }
+                                case "vertex" : {
+                                    this.points[nbPoints] = (new Point(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]))));
+                                    this.nbPoints ++;
+                                    break;
+                                }
+                                case "sphere" : {
+                                    this.shapes.add(new Sphere(new Point(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]))), Double.parseDouble(parts[4])));
+                                    break;
+                                }
+                                case "tri" : {
+                                    Point p1 = this.points[(Integer.parseInt(parts[1]))];
+                                    Point p2 = this.points[(Integer.parseInt(parts[2]))];
+                                    Point p3 = this.points[(Integer.parseInt(parts[3]))];
+                                    this.shapes.add(new Tri(p1, p2, p3));
+                                    break;
+                                }
+                                case "plane" : {
+                                    this.shapes.add(new Plane(new Point(new Triplet(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]))),
+                                            new Vector(new Triplet(Double.parseDouble(parts[4]), Double.parseDouble(parts[5]), Double.parseDouble(parts[6])))));
+                                    break;
+                                }
+                                default : {
+                                    throw new Exception("Incorrect entry");
+                                }
                             }
                         } else {
-                            System.out.println("Nombre incorrect d'arguments pour l'attribut '" + attribute + "'.");
+                            System.out.println("Incorrect number of arguments for attribute " + attribute + "'.");
                         }
                     } else {
-                        System.out.println("Attribut non reconnu : " + attribute);
+                        System.out.println("Unrecognized attribut : " + attribute);
                     }
                 }
             }
