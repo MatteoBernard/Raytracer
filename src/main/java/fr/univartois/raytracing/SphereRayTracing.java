32f4492ff3ebc81d0919ebfb8df4b69c84742dfa
Jean-Alexis Delcambre <ja.delcambre@gmail.com>
2023-10-24 14:22:05 +0200

    Bug fix #22

--- src/main/java/fr/univartois/raytracing/SphereRayTracing.java
+++ src/main/java/fr/univartois/raytracing/SphereRayTracing.java
@@ -1,59 +1,79 @@
 package fr.univartois.raytracing;
 
-import fr.univartois.raytracing.lumiere.ILight;
-import fr.univartois.raytracing.lumiere.PonctualLight;
+
 import fr.univartois.raytracing.numeric.Point;
 import fr.univartois.raytracing.numeric.Triplet;
 import fr.univartois.raytracing.numeric.Vector;
 import fr.univartois.raytracing.numeric.Color;
+import fr.univartois.raytracing.parser.Parser;
 import fr.univartois.raytracing.scenery.Camera;
 import fr.univartois.raytracing.scenery.Scenery;
+import fr.univartois.raytracing.scenery.SceneryBuilder;
 import fr.univartois.raytracing.shape.IShape;
 import fr.univartois.raytracing.shape.Sphere;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SphereRayTracing {
 
-    public void launch (Scenery scene) {
+    public void launch (Scenery scene,String output) {
         BufferedImage image = new BufferedImage(scene.getX(),scene.getY(),BufferedImage.TYPE_INT_RGB);
         Point lookFrom = scene.getCamera().getLookFrom();
-        Point lookAt = scene.getCamera().getLookAt();
-        Vector w = lookFrom.substraction(lookAt);
-        Vector dNorm = w.norm();
-        Vector d = new Vector(new Triplet(
-                w.getTriplet().getX()/dNorm.getTriplet().getX(),
-                w.getTriplet().getY()/dNorm.getTriplet().getY(),
-                w.getTriplet().getZ()/dNorm.getTriplet().getZ()
-        ));
 
         double min = scene.getX()*scene.getY();
-        double t;
+        double t,fovr,pixelHeight,pixelWidth,a,b;
+        Vector w,up,u,v,d;
+
+        w = (scene.getCamera().getLookFrom()).substraction(scene.getCamera().getLookAt());
+        w = w.norm();
+
+        up = scene.getCamera().getUp();
+        u = up.vectorProduct(w);
+        u = u.norm();
+
+        v = w.vectorProduct(u);
+        v = v.norm();
+
+
 
         for (int i=0; i < scene.getX(); i++) {
             for (int j=0; j < scene.getY(); j++) {
                 for (IShape shape : scene.getShapes()) {
                     if (shape instanceof Sphere) {
+
+                        fovr = (scene.getCamera().getFov()*Math.PI)/180;
+                        pixelHeight = Math.tan(fovr/2);
+                        pixelWidth = pixelHeight * ((double) scene.getX() /scene.getY());
+
+                        a = -((double) scene.getX() /2)+(j+0.5)*pixelWidth;
+                        b = ((double) scene.getY() /2)-(i+0.5)*pixelHeight;
+
+
+                        d = ((u.scalarMultiplication(a)).addition(v.scalarMultiplication(b))).substraction(w);
+                        d = d.norm();
+
+
                         t = shape.intersect(lookFrom,d);
-                        if (t != -1 && t < min) min = t;
+                        if (t < min && t != -1) {
+                            min = t;
+                        }
                     }
                 }
 
                 Color col;
-                Point ptAc = new Point(new Triplet(i,j,0));
                 if (min != scene.getX()*scene.getY()) {
-                    Point p = d.addition(scene.getCamera().getLookFrom());
-                    p = p.scalarMultiplication(min);
-                    col = scene.getLights().get(1).getColor();
-                    image.setRGB(i,j,
-                            (int)col.getTriplet().getX()+
-                            (int)col.getTriplet().getY()+
-                            (int)col.getTriplet().getZ()
+                    col = scene.getColors().get("ambient");
+                    System.out.println(min);
+                    image.setRGB(i,j,155
+                            //(int)col.getTriplet().getX()*65536+
+                            //(int)col.getTriplet().getY()*256+
+                            //(int)col.getTriplet().getZ()
                     );
                 }
                 else {
@@ -64,7 +84,7 @@ public class SphereRayTracing {
         }
         try {
             // Retrieve image
-            File outputfile = new File("output.png");
+            File outputfile = new File("src/main/resources/images/"+output);
             ImageIO.write(image, "png", outputfile);
         } catch (IOException e) {
             System.err.println("erreur");
@@ -72,22 +92,25 @@ public class SphereRayTracing {
     }
 
 
-    public static void main(String[] args) {
-        Camera camera = new Camera(
-                new Point(new Triplet(0,0,0)),
-                new Point(new Triplet(0,0,0)),
-                new Point(new Triplet(0,0,0)),
-                1
-        );
-        Scenery scene = new Scenery(
-                camera,
-                new ArrayList<ILight>(),
-                new ArrayList<IShape>(),
-                640,
-                480
-        );
+    public static void main(String[] args) throws Exception {
+        Parser p = new Parser();
+        p.useParser("src/main/resources/generators/1redsph.txt");
+
+        System.out.println(p.getLights());
+
+        SceneryBuilder build = new SceneryBuilder(p.getDimensions()[0],p.getDimensions()[1]);
+        build.setCamera(p.getCamera());
+        build.setLight(p.getLights());
+        build.setShapes(p.getShapes());
+        build.setColors(p.getColors());
+
+        Scenery scene = new Scenery(build.getCamera(),build.getLights(),build.getShapes(),build.getColors(),build.getX(),build.getY());
+
+        for (IShape shape : scene.getShapes()) {
+            System.out.println(shape);
+        }
 
         SphereRayTracing rt = new SphereRayTracing();
-        rt.launch(scene);
+        rt.launch(scene,p.getOutput());
     }
 }
