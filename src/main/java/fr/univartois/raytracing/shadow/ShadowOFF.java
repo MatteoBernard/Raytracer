12e52fd2e59516206adeed5fbebf53671afc17ab
Matteo Bernard <matteo_bernard1@ens.univ-artois.fr>
2023-10-25 14:16:26 +0200

    Singleton pattern to ShadowOFF and ShadowON

--- src/main/java/fr/univartois/raytracing/shadow/ShadowOFF.java
+++ src/main/java/fr/univartois/raytracing/shadow/ShadowOFF.java
@@ -2,6 +2,14 @@ package fr.univartois.raytracing.shadow;
 
 public class ShadowOFF implements ShadowState {
 
+    private static ShadowOFF instance;
 
+    private ShadowOFF() {};
+
+    public static ShadowOFF getInstance() {
+        if (instance == null)
+            instance =  new ShadowOFF();
+        return instance;
+    }
 
 }
--- src/main/java/fr/univartois/raytracing/shadow/ShadowON.java
+++ src/main/java/fr/univartois/raytracing/shadow/ShadowON.java
@@ -1,4 +1,17 @@
 package fr.univartois.raytracing.shadow;
 
 public class ShadowON implements ShadowState {
+
+    private static ShadowON instance;
+
+    private ShadowON() {};
+
+    public static ShadowON getInstance() {
+        if (instance == null)
+            instance =  new ShadowON();
+        return instance;
+    }
+
+
+
 }
