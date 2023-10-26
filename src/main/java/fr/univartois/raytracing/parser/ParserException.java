49a032d1717a90396f2610d662e84fd338f68deb
Matteo Bernard <matteo_bernard1@ens.univ-artois.fr>
2023-10-26 17:29:22 +0200

    adding doc to ParserException

--- src/main/java/fr/univartois/raytracing/parser/ParserException.java
+++ src/main/java/fr/univartois/raytracing/parser/ParserException.java
@@ -1,6 +1,15 @@
 package fr.univartois.raytracing.parser;
 
+/**
+ * Exception class for handling errors that occur during parsing of scene files.
+ */
 public class ParserException extends RuntimeException {
+
+    /**
+     * Constructs a new `ParserException` with the specified error message.
+     *
+     * @param message The error message describing the issue.
+     */
     public ParserException(String message) {
         super(message);
     }
