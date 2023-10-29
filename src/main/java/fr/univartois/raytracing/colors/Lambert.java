666356b31af6a2ffea25eb79cb5dd7e2ea75c147
Antoine Crauser <antoine_crauser@ens.univ-artois.fr>
2023-10-29 11:10:39 +0100

    Doc Lambert.java

--- src/main/java/fr/univartois/raytracing/colors/Lambert.java
+++ src/main/java/fr/univartois/raytracing/colors/Lambert.java
@@ -1,9 +1,15 @@
+/**
+ * The Lambert class implements the Lambert method to calculate the color of surfaces.
+ * The class calculates the light reflection color using Lambert method.
+ *
+ * @author antoine_crauser
+ */
+
 package fr.univartois.raytracing.colors;
 
 import fr.univartois.raytracing.light.ILight;
 import fr.univartois.raytracing.numeric.Color;
 import fr.univartois.raytracing.numeric.Point;
-import fr.univartois.raytracing.numeric.Triplet;
 import fr.univartois.raytracing.numeric.Vector;
 import fr.univartois.raytracing.scenery.Scenery;
 import fr.univartois.raytracing.shape.IShape;
@@ -12,18 +18,45 @@ public class Lambert implements ICalcul {
     private Normal calcul;
     private Scenery scene;
 
+    /**
+     * This constructor constructs an object of Lambert with the Normal calculator.
+     *
+     * @param calcul The Normal calculator for the calculations.
+     */
+
     public Lambert(Normal calcul) {
         this.calcul = calcul;
         this.scene = calcul.getScene();
     }
+    /**
+     * Returns the Calcul of the method.
+     *
+     * @return The calcul.
+     */
 
     public Normal getCalcul() {
         return calcul;
     }
+    /**
+     * Returns the Scene of the method.
+     *
+     * @return The scene.
+     */
+
 
     public Scenery getScene() {
         return scene;
     }
+
+    /**
+     * Use the Lambert method to calculate shading of colors
+     *
+     * @param dlight is the direct source of light for the shading calculation.
+     * @param shape is the shape on which we apply the shading.
+     * @param d is the vector that allow to calculate the intersection point with the shape.
+     * @param t is the double used for calculations.
+     * @return The color of the shading.
+     */
 
     public Color lambertCalcul(ILight dlight,IShape shape,Vector d, double t) {
         Vector n;
@@ -35,9 +68,9 @@ public class Lambert implements ICalcul {
 
         double a;
         Vector ldir = dlight.getVector();
-        if(ldir == null) {
+        if(ldir == null)
                 ldir = (dlight.getCoord()).substraction(p);
-        };
+
         ldir = ldir.norm();
 
         a = n.scalarProduct(ldir);
@@ -48,6 +81,15 @@ public class Lambert implements ICalcul {
 
         return ld;
     }
+
+    /**
+     * It calculates the color of the shape with the Lambert method
+     *
+     * @param shape is the shape for which we calculate the color.
+     * @param d is the vector that allow to calculate the intersection point with the shape.
+     * @param t is the double used for calculations.
+     * @return The color of the shape with the Lambert method.
+     */
     @Override
     public Color colorCalcul(IShape shape, Vector d, double t) {
         Color sum = calcul.colorCalcul(shape,d,t);
